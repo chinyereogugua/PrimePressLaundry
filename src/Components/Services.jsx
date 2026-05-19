96ff36c22d95580d641cbb3f9f7326a981fbd908
chinyereogugua <dorcaschinyere936@gmail.com>
2026-05-19 17:20:54 +0100

    i reworked on the header and the hero component

---
+++ src/Components/Services.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./Css/Services.css"
+
+const Services = () => {
+  return (
+    <main className='services-container'>
+
+    </main>
+  )
+}
+
+export default Services
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import "./Css/Home.css"
 import Header from "../Components/Header.jsx"
 import Hero from "../Components/Hero.jsx"
+import Services from "../Components/Services.jsx"
 
 const Home = () => {
   return (
     <main className='home-container'>
         <Header />
         <Hero />
+        < Services />
     </main>
   )
 }
